0f03973c3dfdcaa79c31019f1db3a806395d50dc
caoduczpsm <nguyencaoduc0922@gmail.com>
2023-05-03 20:59:41 +0700

    Done ui create warehouse

--- app/src/main/java/com/example/catfisharea/activities/admin/WarehouseActivity.java
+++ app/src/main/java/com/example/catfisharea/activities/admin/WarehouseActivity.java
@@ -7,24 +7,37 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AutoCompleteTextView;
+
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.android.app.catfisharea.R;
 import com.android.app.catfisharea.databinding.ActivityWarehouseBinding;
 import com.example.catfisharea.activities.BaseActivity;
+import com.example.catfisharea.adapter.SpinnerAdapter;
 import com.example.catfisharea.adapter.WarehouseAdapter;
 import com.example.catfisharea.listeners.WarehouseListener;
 import com.example.catfisharea.models.Campus;
 import com.example.catfisharea.models.ItemWarehouse;
+import com.example.catfisharea.models.Pond;
+import com.example.catfisharea.models.RegionModel;
 import com.example.catfisharea.models.Warehouse;
 import com.example.catfisharea.ultilities.Constants;
 import com.example.catfisharea.ultilities.PreferenceManager;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class WarehouseActivity extends BaseActivity implements WarehouseListener {
     private ActivityWarehouseBinding mBinding;
@@ -32,6 +45,10 @@ public class WarehouseActivity extends BaseActivity implements WarehouseListener
     private PreferenceManager preferenceManager;
     private WarehouseAdapter warehouseAdapter;
     private List<ItemWarehouse> mWarehouses;
+    private AutoCompleteTextView spinnerPond;
+    private TextInputEditText edtNameWarehouse, edtAceage, edtDescription;
+    private Campus campus;
+    private Pond pondSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +77,142 @@ public class WarehouseActivity extends BaseActivity implements WarehouseListener
         Dialog dialog = openDialog(R.layout.layout_dialog_create_warehouse);
         assert dialog != null;
 
+        TextInputLayout textInputCampus;
+        AutoCompleteTextView spinnerCampus;
+        AppCompatButton btnClose, btnCreate;
+
+        edtNameWarehouse = dialog.findViewById(R.id.edtNameWarehouse);
+        edtAceage = dialog.findViewById(R.id.edtAceage);
+        edtDescription = dialog.findViewById(R.id.edtDescription);
+        spinnerCampus = dialog.findViewById(R.id.spinnerCampus);
+        spinnerPond = dialog.findViewById(R.id.spinnerPond);
+
+        textInputCampus = dialog.findViewById(R.id.textInputCampus);
+
+        btnClose = dialog.findViewById(R.id.btnClose);
+        btnCreate = dialog.findViewById(R.id.btnCreate);
+
+        ArrayList<RegionModel> mCampus = new ArrayList<>();
+        SpinnerAdapter spinnerAdapter = new SpinnerAdapter(this, R.layout.layout_spinner_item, mCampus);
+        spinnerCampus.setAdapter(spinnerAdapter);
+
+        if (preferenceManager.getString(Constants.KEY_TYPE_ACCOUNT).equals(Constants.KEY_REGIONAL_CHIEF)
+                && preferenceManager.getString(Constants.KEY_AREA_ID) != null) {
+            database.collection(Constants.KEY_COLLECTION_CAMPUS)
+                    .whereEqualTo(Constants.KEY_AREA_ID, preferenceManager.getString(Constants.KEY_AREA_ID))
+                    .get().addOnSuccessListener(queryDocumentSnapshots -> {
+                        for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
+                            String id = doc.getId();
+                            String name = doc.getString(Constants.KEY_NAME);
+                            ArrayList<GeoPoint> geoList = (ArrayList<GeoPoint>) doc.get(Constants.KEY_MAP);
+                            Campus campus = new Campus(id, name, geoList, preferenceManager.getString(Constants.KEY_AREA_ID));
+                            mCampus.add(campus);
+                        }
+                        spinnerAdapter.notifyDataSetChanged();
+                    });
+        } else if (preferenceManager.getString(Constants.KEY_TYPE_ACCOUNT).equals(Constants.KEY_ADMIN)) {
+            database.collection(Constants.KEY_COLLECTION_CAMPUS)
+                    .whereEqualTo(Constants.KEY_COMPANY_ID, preferenceManager.getString(Constants.KEY_COMPANY_ID))
+                    .get().addOnSuccessListener(queryDocumentSnapshots -> {
+                        for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
+                            String id = doc.getId();
+                            String name = doc.getString(Constants.KEY_NAME);
+                            String areaId = doc.getString(Constants.KEY_AREA_ID);
+                            ArrayList<GeoPoint> geoList = (ArrayList<GeoPoint>) doc.get(Constants.KEY_MAP);
+                            Campus campus = new Campus(id, name, geoList, areaId);
+                            mCampus.add(campus);
+                        }
+                        spinnerAdapter.notifyDataSetChanged();
+                    });
+        } else {
+            textInputCampus.setVisibility(View.GONE);
+            getDataPond(preferenceManager.getString(Constants.KEY_CAMPUS_ID));
+        }
+
+        spinnerCampus.setOnItemClickListener((parent, view, position, id) -> {
+            campus = (Campus) parent.getItemAtPosition(position);
+            getDataPond(campus.getId());
+        });
+
+        btnCreate.setOnClickListener(view -> {
+            createWarehouse();
+            dialog.dismiss();
+        });
+
+        btnClose.setOnClickListener(view -> dialog.dismiss());
+
         dialog.show();
+    }
+
+    public void getDataPond(String campusId) {
+        ArrayList<RegionModel> mPond = new ArrayList<>();
+        SpinnerAdapter spinnerAdapter = new SpinnerAdapter(this, R.layout.layout_spinner_item, mPond);
+        spinnerPond.setAdapter(spinnerAdapter);
+
+        database.collection(Constants.KEY_COLLECTION_POND)
+                .whereEqualTo(Constants.KEY_CAMPUS_ID, campusId)
+                .get().addOnSuccessListener(pondQuery -> {
+                    for (DocumentSnapshot doc: pondQuery.getDocuments()) {
+                        Map<String, Object> data = doc.getData();
+                        String id = doc.getId();
+                        assert data != null;
+                        Pond pond = new Pond(id, Objects.requireNonNull(data.get(Constants.KEY_NAME)).toString());
+                        mPond.add(pond);
+                    }
+                    spinnerAdapter.notifyDataSetChanged();
+                });
+
+        spinnerPond.setOnItemClickListener((parent, view, position, id) ->
+                pondSelected = (Pond) parent.getItemAtPosition(position));
+    }
+
+    private void createWarehouse() {
+        String name = Objects.requireNonNull(edtNameWarehouse.getText()).toString();
+        String acreage = Objects.requireNonNull(edtAceage.getText()).toString();
+        String description = Objects.requireNonNull(edtDescription.getText()).toString();
+
+        if (!name.isEmpty() && !acreage.isEmpty()) {
+            if (preferenceManager.getString(Constants.KEY_TYPE_ACCOUNT).equals(Constants.KEY_ADMIN)
+                    || preferenceManager.getString(Constants.KEY_TYPE_ACCOUNT).equals(Constants.KEY_REGIONAL_CHIEF)) {
+                if (campus != null && pondSelected != null) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put(Constants.KEY_NAME, name);
+                    data.put(Constants.KEY_ACREAGE, acreage);
+                    data.put(Constants.KEY_DESCRIPTION, description);
+                    data.put(Constants.KEY_CAMPUS_ID, campus.getId());
+                    data.put(Constants.KEY_COMPANY_ID, preferenceManager.getString(Constants.KEY_COMPANY_ID));
+                    data.put(Constants.KEY_POND_ID, pondSelected.getId());
+                    database.collection(Constants.KEY_CAMPUS).document(campus.getId()).get()
+                            .addOnSuccessListener(documentSnapshot -> {
+                                String areaId = documentSnapshot.getString(Constants.KEY_AREA_ID);
+                                data.put(Constants.KEY_AREA_ID, areaId);
+                                database.collection(Constants.KEY_COLLECTION_WAREHOUSE)
+                                        .document()
+                                        .set(data)
+                                        .addOnSuccessListener(task -> onBackPressed());
+                            });
+                }
+            } else {
+                if (preferenceManager.getString(Constants.KEY_CAMPUS_ID) != null && pondSelected != null) {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put(Constants.KEY_NAME, name);
+                    data.put(Constants.KEY_ACREAGE, acreage);
+                    data.put(Constants.KEY_DESCRIPTION, description);
+                    data.put(Constants.KEY_CAMPUS_ID, preferenceManager.getString(Constants.KEY_CAMPUS_ID));
+                    data.put(Constants.KEY_COMPANY_ID, preferenceManager.getString(Constants.KEY_COMPANY_ID));
+                    data.put(Constants.KEY_POND_ID, pondSelected.getId());
+                    database.collection(Constants.KEY_CAMPUS).document(preferenceManager.getString(Constants.KEY_CAMPUS_ID)).get()
+                            .addOnSuccessListener(documentSnapshot -> {
+                                String areaId = documentSnapshot.getString(Constants.KEY_AREA_ID);
+                                data.put(Constants.KEY_AREA_ID, areaId);
+                                database.collection(Constants.KEY_COLLECTION_WAREHOUSE)
+                                        .document().set(data).addOnSuccessListener(task -> onBackPressed());
+                            });
+                }
+
+            }
+
+        }
     }
 
     private void getDataWarehouse() {
